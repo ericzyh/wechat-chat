6159779e5e111fb28000b629a453d6b5972a9436
eviem <343962018@qq.com>
2016-11-24 14:42:34 +0800

    修改无法清空的BUG
    如题：修改无法清空的BUG

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,32 +12,37 @@ var text = '';
 
 var user = {};
 
+var msg = ' ';
 Page({
   data: {
     message : '',
-    text : text
+    text : text,
+    msg : '',
   },
   bindChange: function(e) {
         message = e.detail.value
+        
   },
   //事件处理函数
   add: function(e) {
+    var that = this;
     websocket.send(user.nickName +" : "+ message);
+    message = ' ';
+    that.setData({
+      msg: ' ',
+    });
   },
   
   onLoad: function () {
-    
     var that = this
-
-
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       user = userInfo;
-
       websocket.connect(user, function(res) {
         text = res.data +"\n" + text;
         that.setData({
-          text:text
+          text:text,
+          msg:'\0',
         });
       })
     })
